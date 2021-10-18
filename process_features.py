07633225fe1e949bb2e644e96f77c378af8e2d57
JacobGalajda <jegalajda13@knights.ucf.edu>
2021-10-18 15:51:29 -0400

    Shell for process_features implemented

---
+++ process_features.py
@@ -0,0 +1,24 @@
+'''
+Machine Learning Project: Processing Features
+
+Run python3 process_features.py to watch the magic happen!
+'''
+
+# MODIFY THESE VALUES FOR YOUR SYSTEM
+DATASET_FOLDER = 'mnist/'
+OUTPUT_FOLDER = '.'
+
+# TODO(): Download MNIST
+
+
+# TODO(): Process MNIST
+def processData():
+    pass
+
+# TODO(): Scrub Features
+def scrubData(data):
+    pass
+
+# TODO(): Output Results
+def writeData(data):
+    pass
